9fef579547f0da168e094a4eddf7e685edcc0465
10361 <10361@C22A17H2W65W2.mshome.net>
2020-02-12 20:45:54 -0300

    Atualização model

--- src/Model/FolhaPagamentoModel.java
+++ src/Model/FolhaPagamentoModel.java
@@ -17,17 +17,36 @@ public class FolhaPagamentoModel {
 
     public FolhaPagamentoModel(double salarioBruto) {
         this.salarioBruto = salarioBruto;
- 
-    public double getInss() {
-        if(salarioBruto)
+    }
+    public double getInss(){
+        if(salarioBruto < 1751.82){
+            this.inss = salarioBruto * 0.08;
+        } else if(salarioBruto > 1751.81 && salarioBruto < 2919.73){
+            this.inss = salarioBruto * 0.09;
+        } else if(salarioBruto > 2919.72 && salarioBruto < 5839.46){
+            this.inss = salarioBruto * 0.11;
+        } else if (salarioBruto > 5839.45){
+            inss = 817.66;
+        }
         return inss;
     }
 
     public double getIrrf() {
+        double salarioSInss = salarioBruto - getInss();
+        if(salarioSInss > 1903.98 && salarioSInss < 2826.66){
+            irrf = salarioSInss * 0.075 - 142.80;
+        } else if(salarioSInss > 2826.65 && salarioSInss < 3751.06) {
+            irrf = salarioSInss * 0.15 - 354.8;
+        } else if(salarioSInss > 3751.05 && salarioSInss < 4664.69){
+            irrf = salarioSInss * 0.225 - 636.13;
+        } else if(salarioSInss > 4664.68){
+            irrf = salarioSInss * 0.275 - 869.36;
+        }
         return irrf;
     }
 
     public double getSalarioLiquido() {
+        this.salarioLiquido = salarioBruto - (getIrrf() + getIrrf());
         return salarioLiquido;
     }
     

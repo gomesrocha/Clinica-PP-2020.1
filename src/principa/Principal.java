25bd78793488ebb83ae242a0a9e79a29f15dc174
10361 <10361@C2A49BMCZPK2.acd.grupotiradentes.com>
2020-02-19 19:58:48 -0300

    Código de banco de dados textual em Java

--- src/principa/Principal.java
+++ src/principa/Principal.java
@@ -5,6 +5,17 @@
  */
 package principa;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author 10361
@@ -15,7 +26,19 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        try {
+            Path arquivo = Paths.get("arquivo.txt");
+            BufferedWriter bw = null;
+            
+            Files.createFile(arquivo);
+            bw = new BufferedWriter(new FileWriter(arquivo.toFile(), true));
+            bw.write("Texto");
+            bw.close();
     
+        } catch (IOException ex) {
+            System.out.println("Deu merda no arquivo");
+        }
+
+    }
+
 }
